1b27ee4376f51dafc5edc2d73c936caad62b192f
Luka Julakidze <julayidzeluka@gmail.com>
2026-08-06 14:43:16 -0700

    added DRF, JWT, Swagger, CORS

--- core/settings.py
+++ core/settings.py
@@ -22,9 +22,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'watches',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,3 +110,8 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7)
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+]
